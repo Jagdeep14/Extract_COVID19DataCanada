c48308e8a513575ba3bed79f6e5eb98e8f0549c7
“Aishwarya <aishwaryasharma700@yahoo.com>
2022-02-12 14:45:29 -0800

    Commiting second function

--- aishwarya_file.R
+++ aishwarya_file.R
@@ -15,7 +15,7 @@ request_fun <- function(){
 vaccine_as_per_province <- function(provinceName = 'Canada'){
 	#' Function for returning data frame for the Covid - 19 vaccination in different provinces in Canada.
 	#' 
-	#' Perform data wrangling and cleaning using the API for the Covid - 19 cases in Canada. 
+	#' Performed data wrangling and cleaning using the API for the Covid - 19 cases in Canada. 
 	#' It processes the API and returns the data corresponding to one province 
 	#' which is passed on as the argument. If user passes empty argument, so by default Canada is used which returns the data of whole Canada as a whole.
 	#' The returned data is a data frame and contains the columns including the date of the vaccination, province name, administered vaccine and cumulative vaccine.
@@ -63,5 +63,60 @@ vaccine_as_per_province <- function(provinceName = 'Canada'){
 		avaccine_data <- avaccine_data %>% 
 			filter(tolower(province) == tolower(provinceName))
 		return(avaccine_data)
+	}
+}
+
+
+cases_as_per_province <- function(provinceName = 'Canada'){
+	#' Function for returning data frame for the Covid - 19 cases in different provinces in Canada.
+	#' 
+	#' Performed data wrangling and cleaning using the API for the Covid - 19 cases in Canada. 
+	#' It processes the API and returns the data corresponding to one province 
+	#' which is passed on as the argument. If user passes empty argument, so by default Canada is used which returns the data of whole Canada as a whole.
+	#' The returned data is a data frame and contains the columns including the date of the cases, province name, cases and cumulative cases.
+	#'    
+	#' @param provinceName a character/ string depicting the name of the province
+	#' 
+	#' @return Data frame for the Covid - 19 cases corresponding to a particular province
+	#' 
+	#' @examples 
+	#' cases_as_per_province('Alberta')
+	
+	prov = c("Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Nunavut", "Northwest Territories", "Ontario", "Prince Edward 	                Island", "Quebec", "Saskatchewan", "Yukon", "Canada")
+	`%!in%` <- Negate(`%in%`)
+	if(tolower(provinceName) %!in% tolower(prov)){
+		stop("Please enter a valid province name that too in its full form!")
+	}
+	
+	# Fetching the data using the URL
+	request <- request_fun()
+	
+	# Reading the data as per JSON and then saving that in data frame
+	json_data <- content(request, as  = "parse")
+	cases <- json_data$cases
+	cases_data <- data.frame()
+	for(i in 1:length(cases)){
+		cases_data <- rbind(cases_data, data.frame(cases[[i]]))
+	}
+	
+	# Data Cleaning
+	cases_data <- cases_data %>% 
+		rename('date' = 'date_report') %>%
+		mutate(date=as.Date(date, format = "%d-%m-%Y"))%>%
+		mutate(province = replace(province, province %in% c("BC"), "British Columbia"),
+					 province = replace(province, province %in% c("NL"), "Newfoundland and Labrador"),
+					 province = replace(province, province %in% c("NWT"), "Northwest Territories"),
+					 province = replace(province, province %in% c("PEI"), "Prince Edward Island"))
+	
+	cases_data <- cases_data[, c(3, 4, 1, 2)]
+	
+	# Data Wrangling
+	
+	if(tolower(provinceName) == 'canada'){
+		return(cases_data)
+	} else {
+		cases_data <- cases_data %>% 
+			filter(tolower(province) == tolower(provinceName))
+		return(cases_data)
 	}
 }
