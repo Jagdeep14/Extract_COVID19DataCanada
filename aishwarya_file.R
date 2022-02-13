a421b3f384a53c38fd3bbb652e39ae5f5e2f97b1
“Aishwarya <aishwaryasharma700@yahoo.com>
2022-02-12 16:04:10 -0800

    Commiting yearly cases function

--- aishwarya_file.R
+++ aishwarya_file.R
@@ -120,3 +120,64 @@ cases_as_per_province <- function(provinceName = 'Canada'){
 		return(cases_data)
 	}
 }
+
+
+
+Yearlycases_as_per_province <- function(provinceName = 'Canada',yearpassed='2020'){
+	
+	#' Function for returning data frame for the Covid - 19 cases in different provinces as per the year specified in Canada.
+	#' 
+	#' Performed data wrangling and cleaning using the API for the Covid - 19 cases in Canada. 
+	#' It processes the API and returns the data corresponding to one province 
+	#' which is passed on as the argument. If user passes empty argument, so by default Canada is used which returns the data of whole Canada as a whole.
+	#' Also, the data is returned as per the year. By default the year chosen is 2020.
+	#' The returned data is a data frame and contains the columns including the date of the cases, province name, cases and cumulative cases for the year 	    #' specified
+	#'    
+	#' @param provinceName a character/ string depicting the name of the province
+	#' 
+	#' @return Data frame for the Covid - 19 cases corresponding to a particular province and year
+	#' 
+	#' @examples 
+	#' Yearlycases_as_per_province('Alberta','2020')
+	
+	prov = c("Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Nunavut", "Northwest Territories", "Ontario", "Prince Edward 	                Island", "Quebec", "Saskatchewan", "Yukon", "Canada")
+	`%!in%` <- Negate(`%in%`)
+	if(tolower(provinceName) %!in% tolower(prov)){
+		stop("Please enter a valid province name that too in its full form!")
+	}
+	
+	# Fetching the data using the URL
+	request <- request_fun()
+	
+	# Reading the data as per JSON and then saving that in data frame
+	json_data <- content(request, as  = "parse")
+	cases <- json_data$cases
+	cases_data <- data.frame()
+	for(i in 1:length(cases)){
+		cases_data <- rbind(cases_data, data.frame(cases[[i]]))
+	}
+	
+	# Data Cleaning
+	cases_data <- cases_data %>% 
+		rename('date' = 'date_report') %>%
+		mutate(date=as.Date(date, format = "%d-%m-%Y"))%>%
+		mutate(province = replace(province, province %in% c("BC"), "British Columbia"),
+					 province = replace(province, province %in% c("NL"), "Newfoundland and Labrador"),
+					 province = replace(province, province %in% c("NWT"), "Northwest Territories"),
+					 province = replace(province, province %in% c("PEI"), "Prince Edward Island")) %>%
+		mutate(year = format(date, format = "%Y")) 
+	
+	cases_data <- cases_data[, c(3, 4, 1, 2, 5)]
+	
+	# Data Wrangling
+	
+	if(tolower(provinceName) == 'canada'){
+		cases_data <- cases_data %>% 
+			filter(year==yearpassed)
+		return(cases_data)
+	} else {
+		cases_data <- cases_data %>% 
+			filter(tolower(province) == tolower(provinceName),year==yearpassed)
+		return(cases_data)
+	}
+}
