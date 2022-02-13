a7c85db79e655486339085502aa56d2dad7dadbb
“Aishwarya <aishwaryasharma700@yahoo.com>
2022-02-12 16:52:27 -0800

    Commiting function for yearly cumulative cases

--- aishwarya_file.R
+++ aishwarya_file.R
@@ -3,7 +3,7 @@ library(jsonlite)
 library(dplyr)
 library(docstring)
 
-
+# Function for requesting using the API
 request_fun <- function(){
 	#' Function to make request to API and returns the response
 	url <- 'https://api.opencovid.ca/timeseries'
@@ -12,6 +12,7 @@ request_fun <- function(){
 }
 
 
+# Function for getting information about the vaccine as per the province
 vaccine_as_per_province <- function(provinceName = 'Canada'){
 	#' Function for returning data frame for the Covid - 19 vaccination in different provinces in Canada.
 	#' 
@@ -67,6 +68,7 @@ vaccine_as_per_province <- function(provinceName = 'Canada'){
 }
 
 
+# Function for getting the information about the cases as per the province
 cases_as_per_province <- function(provinceName = 'Canada'){
 	#' Function for returning data frame for the Covid - 19 cases in different provinces in Canada.
 	#' 
@@ -122,7 +124,7 @@ cases_as_per_province <- function(provinceName = 'Canada'){
 }
 
 
-
+# Function for getting the information as per the province and year	
 Yearlycases_as_per_province <- function(provinceName = 'Canada',yearpassed='2020'){
 	
 	#' Function for returning data frame for the Covid - 19 cases in different provinces as per the year specified in Canada.
@@ -134,6 +136,7 @@ Yearlycases_as_per_province <- function(provinceName = 'Canada',yearpassed='2020
 	#' The returned data is a data frame and contains the columns including the date of the cases, province name, cases and cumulative cases for the year 	    #' specified
 	#'    
 	#' @param provinceName a character/ string depicting the name of the province
+	#' @param yearpassed a character/ string depicting the year
 	#' 
 	#' @return Data frame for the Covid - 19 cases corresponding to a particular province and year
 	#' 
@@ -183,6 +186,7 @@ Yearlycases_as_per_province <- function(provinceName = 'Canada',yearpassed='2020
 }
 
 
+# Function for providing details of the cumulative vaccines as per the province
 cumulativevaccine_as_per_province <- function(provinceName = 'Canada'){
 	#' Function for returning data frame for the Covid - 19 vaccination which is cumulative in different provinces in Canada.
 	#' 
@@ -234,5 +238,65 @@ cumulativevaccine_as_per_province <- function(provinceName = 'Canada'){
 		cvaccine_data <- cvaccine_data %>% 
 			filter(tolower(province) == tolower(provinceName))
 		return(cvaccine_data)
+	}
+}
+
+
+# Function for providing the detail regarding the details of cumulative vaccine as per the year and province
+Yearlycumulative_vaccine <- function(provinceName = 'Canada', cumulativeyear='2020'){
+	#' Function for returning data frame for the Covid - 19 vaccination which is cumulative in different provinces in Canada.
+	#' 
+	#' Performed data wrangling and cleaning using the API for the Covid - 19 cases in Canada. 
+	#' It processes the API and returns the data corresponding to one province 
+	#' which is passed on as the argument. If user passes empty argument, so by default Canada is used which returns the data of whole Canada as a whole.
+	#' The returned data is a data frame and contains the columns including the date of the vaccination, province name,cumulative vaccine and cvaccine.
+	#'    
+	#' @param provinceName a character/ string depicting the name of the province
+	#' @param cumulativeyear a character/ string for the year 
+	#'
+	#' @return Data frame for the Covid - 19 cummulative vaccines corresponding to a particular province and year
+	#' 
+	#' @examples 
+	#' Yearlycumulative_vaccine('Alberta','2020')
+	
+	prov = c("Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Nunavut", "Northwest Territories", "Ontario", "Prince Edward 	                Island", "Quebec", "Saskatchewan", "Yukon", "Canada")
+	`%!in%` <- Negate(`%in%`)
+	if(tolower(provinceName) %!in% tolower(prov)){
+		stop("Please enter a valid province name that too in its full form!")
+	}
+	
+	# Fetching the data using the URL
+	request <- request_fun()
+	
+	# Reading the data as per JSON and then saving that in data frame
+	json_data <- content(request, as  = "parse")
+	cvaccine_json <- json_data$cvaccine
+	cvaccine_year <- data.frame()
+	for(i in 1:length(cvaccine_json)){
+		cvaccine_year <- rbind(cvaccine_year, data.frame(cvaccine_json[[i]]))
+	}
+	
+	# Data Cleaning
+	cvaccine_year <- cvaccine_year %>% 
+		rename('date' = 'date_vaccine_completed') %>%
+		mutate(date=as.Date(date, format = "%d-%m-%Y"))%>%
+		mutate(province = replace(province, province %in% c("BC"), "British Columbia"),
+					 province = replace(province, province %in% c("NL"), "Newfoundland and Labrador"),
+					 province = replace(province, province %in% c("NWT"), "Northwest Territories"),
+					 province = replace(province, province %in% c("PEI"), "Prince Edward Island")) %>%
+		mutate(year = format(date, format = "%Y")) 
+	
+	cvaccine_year <- cvaccine_year[, c(3, 4, 1, 2, 5)]
+	
+	# Data Wrangling
+	
+	if(tolower(provinceName) == 'canada'){
+		cvaccine_year <- cvaccine_year %>% 
+			filter(year==cumulativeyear)
+		return(cvaccine_year)
+	} else {
+		cvaccine_year <- cvaccine_year %>% 
+			filter(tolower(province) == tolower(provinceName), year == cumulativeyear)
+		return(cvaccine_year)
 	}
 }
